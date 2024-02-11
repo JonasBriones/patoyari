9834f0bb4de7717ff5a73fa9efb123f62792db02
Jonathan Briones <jonathanbriones@G500606MAC697.local>
2024-02-11 19:11:58 -0300

    una prueba

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,32 @@ function App() {
 
 
   return (
-    <>
-      <h1>matrimonio</h1>
-    </>
+    <div className='container'>
+
+      <div className='row'>
+        <div className='col'>
+          <h1 className='display-1'>Patricio y Yaritza</h1>
+        </div>
+        <div className='col'> 
+          <p>esto es un parrafo</p>
+          </div>
+      </div>
+
+      <footer className="fixed-bottom w-100 p-3">
+          <div className='container row'>
+            <div className='col'>
+              <div className='container'>
+                <h1 className='name_espacial'>ahi va el logo</h1>
+              </div>
+            </div>
+            <div className='col text-end'>
+              <div className='container'>
+                testttttttt
+              </div>
+            </div> 
+          </div>
+      </footer>
+    </div>
   )
 }
 
