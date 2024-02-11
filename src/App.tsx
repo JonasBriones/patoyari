9d968eb12d0f863351eba10b46005b8b6351e06c
Jonathan Briones <jonathanbriones@G500606MAC697.local>
2024-02-11 19:30:33 -0300

    modificiaciones en el

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
       <div className='row'>
         <div className='col'>
-          <h1 className='display-1'>Patricio y Yaritza</h1>
+          <h1 className='titulo_eapacial'>Patricio y Yaritza</h1>
         </div>
         <div className='col'> 
           <p>esto es un parrafo</p>
@@ -17,15 +17,17 @@ function App() {
       </div>
 
       <footer className="fixed-bottom w-100 p-3">
-          <div className='container row'>
+          <div className='d-flex row'>
             <div className='col'>
               <div className='container'>
-                <h1 className='name_espacial'>ahi va el logo</h1>
+                <h1 className='d-flex name_espacial'>Link lista de novios</h1>
               </div>
             </div>
-            <div className='col text-end'>
+            <div className='col'>
               <div className='container'>
-                testttttttt
+                <div className='d-flex name_espacial text-end'>Link formulario 
+                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSeTTLzOFITJpoONTLCDFHHlUTu2pu1L6NXikIKrxa9qfkMh-A/viewform?usp=sf_link">aqui</a>
+                </div>
               </div>
             </div> 
           </div>
